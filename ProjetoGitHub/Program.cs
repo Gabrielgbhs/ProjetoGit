a59cc5c8c1961bdf6c51ee989b63337d79caf86a
Gabriel Barbosa <ti02@itb.ind.br>
2025-07-29 07:32:31 -0300

    feat: Classe produto

--- ProjetoGitHub/Program.cs
+++ ProjetoGitHub/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ProjetoGitHub;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var produto = new Produto();
+        produto.Descricao = "Teclado";
+        produto.Id = 1;
+
+        Console.WriteLine($"Produto: {produto.Descricao}");
+    }
+}
